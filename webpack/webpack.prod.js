e7f3eae9012f94aa2dea60f6de3128d77bc06420
Arnoldo <arnoldo.cortez@theksquaregroup.com>
2023-03-12 16:50:09 -0600

    optimize css for production

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -1,11 +1,30 @@
 const common = require("./webpack.common.js");
 const { merge } = require("webpack-merge");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 
 module.exports = merge(common, {
   mode: "production",
   output: {
     filename: "js/[name].[contenthash:12].js",
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [
+      "...",
+      new CssMinimizerPlugin({
+        minimizerOptions: {
+          preset: [
+            "default",
+            {
+              discardComments: {
+                removeAll: true,
+              },
+            },
+          ],
+        },
+      }),
+    ],
   },
   module: {
     rules: [
