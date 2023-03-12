d6aa6ba6274f34954e1e6783b99b17adbe956a5a
Arnoldo <arnoldo.cortez@theksquaregroup.com>
2023-03-12 16:03:50 -0600

    create separate directories when building

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -5,7 +5,7 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 module.exports = merge(common, {
   mode: "production",
   output: {
-    filename: "[name].[contenthash:12].js",
+    filename: "js/[name].[contenthash:12].js",
   },
   module: {
     rules: [
@@ -17,7 +17,7 @@ module.exports = merge(common, {
   },
   plugins: [
     new MiniCssExtractPlugin({
-      filename: "[name].[contenthash:12].css",
+      filename: "css/[name].[contenthash:12].css",
     }),
   ],
 });
