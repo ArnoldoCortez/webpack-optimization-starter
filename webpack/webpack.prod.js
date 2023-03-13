55915aa3c03c6b4f6cda494085ffb48e98015235
Arnoldo <arnoldo.cortez@theksquaregroup.com>
2023-03-13 16:09:46 -0600

    analyze with bundle analyzer and extract heavy dependencies into separated bundles

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -89,6 +89,20 @@ module.exports = merge(common, {
         ],
       }),
     ],
+    splitChunks: {
+      cacheGroups: {
+        jquery: {
+          test: /[\\/]node_modules[\\/]jquery[\\/]/,
+          chunks: "initial",
+          name: "jquery",
+        },
+        bootstrap: {
+          test: /[\\/]node_modules[\\/]bootstrap[\\/]/,
+          chunks: "initial",
+          name: "bootstrap",
+        },
+      },
+    },
   },
   module: {
     rules: [cssRule, cssModuleRule, lessRule, sassRule, imageRule],
