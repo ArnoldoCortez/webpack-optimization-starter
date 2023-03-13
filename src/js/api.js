311ac1424eb0074f6d553f024d7cdd65e7cf4bda
Arnoldo <arnoldo.cortez@theksquaregroup.com>
2023-03-13 12:19:40 -0600

    generating webp images out of jpeg or png

--- src/js/api.js
+++ src/js/api.js
@@ -2,9 +2,9 @@ export function getMotivationalPictures() {
   return new Promise((resolve) => {
     setTimeout(() => {
       const mockedResponse = [
-        "images/motivational-pictures/motivation1.jpg",
-        "images/motivational-pictures/motivation2.jpg",
-        "images/motivational-pictures/motivation3.jpg",
+        "images/motivational-pictures/motivation1.webp",
+        "images/motivational-pictures/motivation2.webp",
+        "images/motivational-pictures/motivation3.webp",
       ];
       resolve(mockedResponse);
     }, 700);
